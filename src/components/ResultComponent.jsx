e891e62e14a5f3222e4a958fedeb764e19f162ad
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-05-20 18:13:27 +0530

    Update ResultComponent.jsx

--- src/components/ResultComponent.jsx
+++ src/components/ResultComponent.jsx
@@ -1,20 +1,47 @@
-import {Component } from "react";
+import { Component } from "react";
 import '../QuizComponent.css';
-class ResultComponent extends Component{
-    render(){
+import PropTypes from 'prop-types';
+
+class ResultComponent extends Component {
+    handlePlayAgain = () => {
+        this.props.onPlayAgain();
+    }
+
+    handleBackToHome = () => {
+        this.props.onBackToHome();
+    }
+
+    render() {
+        const { totalQuestions, attemptedQuestions, correctAnswers } = this.props;
+        const wrongAnswers = attemptedQuestions - correctAnswers;
+        const scorePercentage = totalQuestions ? (correctAnswers / totalQuestions) * 100 : 0;
+
         return (
             <div className="qst-pad">
                 <h1>Result</h1>
-                <h2>You need more practice</h2>
-                <h1>You score is 20%</h1>
-                <h3>Total number od questions                      15</h3>
-                <h3>Number of attempted questions                    </h3>
-                <h3>Number of correct answers                        </h3>
-                <h3>Number of wrong answers                          </h3>
-                <button className="btn-1">Play Again</button>
-                <button className="btn-2">Back to home </button>
+                {scorePercentage >= 50 ? (
+                    <h2>Congratulations! You passed the quiz.</h2>
+                ) : (
+                    <h2>You need more practice</h2>
+                )}
+                <h1>You score is {scorePercentage.toFixed(2)}%</h1>
+                <h3>Total number of questions: {totalQuestions}</h3>
+                <h3>Number of attempted questions: {attemptedQuestions}</h3>
+                <h3>Number of correct answers: {correctAnswers}</h3>
+                <h3>Number of wrong answers: {wrongAnswers}</h3>
+                <button className="btn-1" onClick={this.handlePlayAgain}>Play Again</button>
+                <button className="btn-2" onClick={this.handleBackToHome}>Back to home</button>
             </div>
-        )
+        );
     }
 }
+
+ResultComponent.propTypes = {
+    totalQuestions: PropTypes.number.isRequired,
+    attemptedQuestions: PropTypes.number.isRequired,
+    correctAnswers: PropTypes.number.isRequired,
+    onPlayAgain: PropTypes.func.isRequired,
+    onBackToHome: PropTypes.func.isRequired
+};
+
 export default ResultComponent;
